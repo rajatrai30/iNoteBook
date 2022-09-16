b7e364071095953b0926666768d0d142a49a3026
rajatrai30 <mailmerairajat@gmail.com>
2022-09-16 23:09:01 +0530

    concurrent run

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,3 @@
-// const connectToMongo = require('./db');
-// const express = require('express');
-
-// connectToMongo();
-// const app = express()
-// const port = 5000;
-// var cors = require('cors')
-
-// app.use(cors())
-// app.use(express.json())
-
-// // Available routes
-// app.use('/api/auth', require('./routes/auth'));
-// app.use('/api/notes', require('./routes/notes'));
-
-
-// app.listen(port, () => {
-//   console.log(`iNoteBook listening on port http://localhost:${port}`)
-// })
-
-// const connectToMongo = require('./db');
-
-
 const mongoose = require('mongoose');
 const express = require('express');
 
